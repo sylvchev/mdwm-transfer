988bca93dd4e19c154aa32fae16839b3a24be14f
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2020-07-06 18:43:00 +0200

    ajout des tests

---
+++ mdwm/tests/test_mdwm.py
@@ -0,0 +1,50 @@
+from ..mdwm import MDWM
+import numpy as np
+
+def generate_cov(Nt, Ne):
+    """Generate a set of cavariances matrices for test purpose."""
+    rs = np.random.RandomState(1234)
+    diags = 2.0 + 0.1 * rs.randn(Nt, Ne)
+    A = 2 * rs.rand(Ne, Ne) - 1
+    A /= np.atleast_2d(np.sqrt(np.sum(A**2, 1))).T
+    covmats = np.empty((Nt, Ne, Ne))
+    for i in range(Nt):
+        covmats[i] = np.dot(np.dot(A, np.diag(diags[i])), A.T)
+    return covmats
+
+
+def test_MDWM_init():
+    m = MDWM()
+
+    m = MDWM(L=0.5)
+    m = MDWM(metric='riemann')
+
+    with pytest.raises(TypeError):
+        MDWM(metric=42)
+
+    # with pytest.raises(ValueError):
+    #     MDWM(L=-0.5)
+
+
+def test_MDWM_fit():
+    X = generate_cov(10, 3)
+    y = np.array([0, 1]).repeat(5)
+    X_domain = generate_cov(10, 3)
+    y_domain = np.array([0, 1]).repeat(5)
+    mdwm = MDWM(L=0.5)
+    mdwm.fit(X, y, X_domain, y_domain)
+
+def test_MDWM_fit():
+    X = generate_cov(10, 3)
+    y = np.array([0, 1]).repeat(5)
+    X_domain = generate_cov(10, 3)
+    y_domain = np.array([0, 1]).repeat(5)
+    
+    mdwm = MDWM(L=0.5)
+    mdwm.fit(X, y, X_domain, y_domain)
+    mdwm.predict(X)
+    mdwm.predict_proba(X)
+
+    mdwm = MDWM(L=0.5, n_jobs=2)
+    mdwm.fit(X, y, X_domain, y_domain)
+    mdwm.predict(X)    
